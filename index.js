64af1ecbdc5e38468338270882f06606487e0e5e
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-02-04 23:26:36 +0300

    i did my hw

---
+++ index.js
@@ -0,0 +1,9 @@
+var a=7
+var b=5
+ console.log("a = ",a,"b = ",b);
+ console.log("cheack a > b",a > b);
+ console.log("cheack b < a",b < a);
+ console.log("cheack a >= b",a >= b);
+ console.log("cheack b <= a",b <= a);
+ console.log("cheack a == b",(a = 4) == (b = 4));
+ console.log("cheack a != b",(a = 2) != (b = 5));
